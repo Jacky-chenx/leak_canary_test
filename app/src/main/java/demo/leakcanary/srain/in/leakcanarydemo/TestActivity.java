ecfd57a72c718e849a1aeb29287bb2aaae90fc21
ChenXiong <chenxiong@tp-link.net>
2016-03-28 09:55:47 +0800

    first_commit

---
+++ app/src/main/java/demo/leakcanary/srain/in/leakcanarydemo/TestActivity.java
@@ -0,0 +1,22 @@
+package demo.leakcanary.srain.in.leakcanarydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        TextView textView = (TextView) findViewById(R.id.test_text_view);
+
+        TestDataModel.getInstance().setRetainedTextView(textView);
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        TestDataModel.getInstance().setRetainedTextView(null);
+    }
+}
